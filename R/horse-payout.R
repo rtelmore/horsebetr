c1d36f0ac7a1b09fddb68fdba21ea47263a40be4
Ryan Elmore <rtelmore@gmail.com>
2016-03-10 13:36:25 -0700

    horse-payout refactored

--- R/horse-payout.R
+++ R/horse-payout.R
@@ -7,21 +7,14 @@
 #' @return A tbl_df object with name, horse, amount, and type of bet
 #' @examples
 #' horse_payout(bet_df = total_bets,
-#'              results = c("Barbaro", "Secretariat", "Mr Ed"))
+#'              results = c("Barbaro", "Secretariat")
 horse_payout <- function(bet_df, results){
   n_bets <- dim(bet_df)[1]
   mult <- horse_multiplier(bet_df, n_factor = sum(results %in% bet_df$horse))
-  win_payouts <- rep(0, n_bets)
-  for (i in 1:n_bets) {
-    if(bet_df$horse[i] %in% results) {
-       bet_multiplier <- filter(mult, horse == bet_df$horse[i]) %>%
-        select(multiplier) %>%
-        .[["multiplier"]]
-      win_payouts[i] <- bet_df$amount[i]*(bet_multiplier + 1)
-    }
-  }
-  bet_df$payouts <- win_payouts
-  final_payouts <- filter(bet_df, payouts > 0) %>%
+  final_payouts <- left_join(bet_df, mult, by = "horse") %>%
+    mutate(winner = bet_df$horse %in% results) %>%
+    filter(winner == TRUE) %>%
+    mutate(payout = amount*(multiplier + 1)) %>%
     group_by(bettor, horse)
   return(final_payouts)
 }
