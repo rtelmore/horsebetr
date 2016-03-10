bcf05facbc29e9599d5922acaf40c1adf96ebb55
Ryan Elmore <rtelmore@gmail.com>
2016-03-10 12:16:40 -0700

    adding comments

--- R/horse-multiplier.R
+++ R/horse-multiplier.R
@@ -1,5 +1,14 @@
+#' Determine the odds (multipliers) for the various bets
+#'
+#' This function returns the horse, total amount bet, and a function of the
+#'   odds for the horse winning
+#'
+#' @param bet_df A tbl_df object with name, horse, amount, and type of bet
+#' @return A tbl_df object with horse, total amount bet, and odds multiplier
+#' @examples
+#' horse_multiplier(bet_df = total_bets, kFactor)
 horse_multiplier <- function(bet_df, kFactor){
-  horses <- unique(bet_df$horse)
+  horses <- unique(select(bet_df, horse))
   bet <- 1:length(horses)
   ## amount bet per horse
   for (horse in bet){
