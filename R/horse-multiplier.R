10e1dacd4fed7ed9a3f8f3ab63872298a0fd5c1b
Ryan Elmore <rtelmore@gmail.com>
2016-03-10 12:59:48 -0700

    new horse-multiplier

--- R/horse-multiplier.R
+++ R/horse-multiplier.R
@@ -6,14 +6,11 @@
 #' @param bet_df A tbl_df object with name, horse, amount, and type of bet
 #' @return A tbl_df object with horse, total amount bet, and odds multiplier
 #' @examples
-#' horse_multiplier(bet_df = total_bets, kFactor)
-horse_multiplier <- function(bet_df, kFactor){
-  horses <- unique(select(bet_df, horse))
-  bet <- 1:length(horses)
-  ## amount bet per horse
-  for (horse in bet){
-    bet[horse] <- sum(bet_df$amount[bet_df$horse == horses[horse]])
-  }
-  mult <- (sum(bet_df$amount)/kFactor - bet)/bet
-  return(data.frame(horses = horses, total = bet, multiplier = mult))
+#' horse_multiplier(bet_df = total_bets, n_factor = 3)
+horse_multiplier <- function(bet_df, n_factor){
+  total_bet <- sum(bet_df$amount)
+  bet_by_horse <- group_by(bet_df, horse) %>%
+    summarize(total = sum(amount)) %>%
+    mutate(muliplier = (total_bet/n_factor - total)/total)
+  return(bet_by_horse)
 }
