7f505b3397c18fe12dd129eb39165f80337468fb
Ryan Elmore <rtelmore@gmail.com>
2016-03-10 13:36:42 -0700

    all-payouts with dplyr magik

--- R/all-payouts.R
+++ R/all-payouts.R
@@ -1,6 +1,6 @@
 all_payouts <- function(bet_df, results){
-  win <- horse_payout(bet_df[bet_df$type == 'win', ], results[1])
-  place <- horse_payout(bet_df[bet_df$type == 'place', ], results[1:2])
-  show <- horse_payout(bet_df[bet_df$type == 'show', ], results[1:3])
+  win <- horse_payout(filter(bet_df, type == "win"), results[1])
+  place <- horse_payout(filter(bet_df, type == "place"), results[1:2])
+  show <- horse_payout(filter(bet_df, type == "show"), results[1:3])
   return(list(win = win, place = place, show = show))
 }
