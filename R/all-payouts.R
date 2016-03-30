45a3426d0009aab67fec23ba896468b8f767a0e9
Ryan Elmore <rtelmore@gmail.com>
2016-03-30 10:01:16 -0600

    uh oh

--- R/all-payouts.R
+++ R/all-payouts.R
@@ -8,8 +8,8 @@
 #' @examples
 #' all_payouts(bet_df = all_bets, results)
 all_payouts <- function(bet_df, results){
-  win <- horse_payout(filter(bet_df, type == "win"), results[1])
-  place <- horse_payout(filter(bet_df, type == "place"), results[1:2])
-  show <- horse_payout(filter(bet_df, type == "show"), results[1:3])
+  win <- horse_payout(filter(bet_df, type == "Win"), results[1])
+  place <- horse_payout(filter(bet_df, type == "Place"), results[1:2])
+  show <- horse_payout(filter(bet_df, type == "Show"), results[1:3])
   return(list(win = win, place = place, show = show))
 }
